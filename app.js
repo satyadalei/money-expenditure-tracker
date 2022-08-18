6fe0c84d2242d9ccca4faa8d035a31ba70d9b067
satyanarayan453 <74782403+satyanarayan453@users.noreply.github.com>
2022-08-18 10:38:08 +0530

    adding user authentication and setting up cookies and session

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 // required modules
+const dotenv = require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const mongoStore = require("connect-mongo");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+// mongoose connections
+const mongodb_url = process.env.MONGO_DB_URL;
+const mongodbOptions = { useNewUrlParser: true }
+mongoose.connect(mongodb_url, mongodbOptions).then(function(){
+    console.log("MongoDB database is connected successfully");
+});
 
 // use and settings of modules
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(session({
+    secret: process.env.SECRET,
+    resave:false,
+    saveUninitialized:false,
+    store: mongoStore.create({
+        mongoUrl: mongodb_url,
+        collectionName: "myAllSession"
+    })
+}))
 
-// mongoose connections
-mongoose.connect("mongodb://localhost:27017/moneyDB", { useNewUrlParser: true });
+
 
 const moneySchema = new mongoose.Schema({
     fname : String,
@@ -34,6 +54,7 @@ app.get("/registration",function(req,res){
     res.render("registration");
 });
 app.get("/individual/:id", function(req,res){
+    if(req.session.isAuth === true){
     userId = req.params.id;
     List.findOne({_id:userId},function(err,foundUser){
         if(err){
@@ -45,36 +66,80 @@ app.get("/individual/:id", function(req,res){
                 sum = sum + parseInt(item.amount) ;
                 });
                 res.render("individual", {user:foundUser, sumTotal:sum});
+                console.log(req.session.user_ID);
             }
         }
-    })
+    })               
+    }else{
+        res.redirect("/login");
+    }
 });
 app.post("/registration", function (req, res) {
-    const newList = new List({
-        fname : req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        password: req.body.confirmpsw
-    });
-    newList.save(function(err){
+    const userFname = req.body.fname ;
+    const userLname = req.body.lname ;
+    const userEmail = req.body.email ;
+    const userPassword =  req.body.confirmpsw ;
+    List.findOne({email:userEmail},function(err,foundUser){
         if(err){
             console.log(err)
         }else{
-            res.redirect("/login");
+            if(foundUser){
+                // this will send client a message that same mail id already exists and provide link to login or signin
+                const messageString = "<h1>This user with same mail_ID already exists. Please try another one.</h1>" ;
+                const regis_LinkUUrl = "/registration" ;
+                const login = "/login" ;
+                const combinedUrl = "<a href="+regis_LinkUUrl +"> Registration </a>"+ "Or" +
+                                    "<a href="+ login +"> Login </a>"
+                res.send(messageString + combinedUrl);
+            }else{
+                bcrypt.hash(userPassword, saltRounds,async function(err,hashedPassword){
+                    if(err){console.log(err)}else{
+                        newList = new List({
+                            fname : userFname,
+                            lname: userLname,
+                            email: userEmail,
+                            password: hashedPassword
+                        });
+                        await newList.save(function(err,savedUser){
+                            if(err){
+                                console.log(err)
+                            }else{
+                                req.session.isAuth = true ;
+                                res.redirect(`/individual/${savedUser._id}`);
+                            }
+                        });
+                    }
+                })
+            }
         }
     });
+    
 });
 app.post("/login",function(req,res){
     const userEmail = req.body.email ;
+    const userPassword = req.body.psw ;
     List.findOne({email:userEmail},function(err,foundUser){
         if(err){
             console.log(err)
         }else{
             if(!foundUser){
-                res.send("This user does not exists")
+                const messageString = "<h1>This user does not exists</h1>" ;
+                const regis_LinkUUrl = "/registration" ;
+                const login = "/login" ;
+                const combinedUrl = "<a href="+regis_LinkUUrl +"> Registration </a>"+ "Or" +
+                                    "<a href="+ login +"> Login </a>"
+                res.send(messageString + combinedUrl);
             }else{
-                const userId = foundUser._id ;
-                res.redirect(`/individual/${userId}`);
+                bcrypt.compare(userPassword, foundUser.password , function(err, passwordConfirmed) {
+                    if(err){console.log(err)}else{
+                        if(passwordConfirmed === true){
+                            req.session.isAuth = true ;
+                            req.session.user_ID = foundUser._id ;
+                            const userId = foundUser._id ;
+                            res.redirect(`/individual/${userId}`);
+                        }
+                    }
+                });
             }
         }
     })
@@ -141,11 +206,17 @@ app.post("/delete", function(req,res){
         }
     });
 });
-
+app.post("/logout", function(req,res){
+    req.session.destroy(function(err){
+       if(err){console.log(err)}else{
+        res.redirect("/");
+       }
+    })
+});
 
 
 
 
 app.listen(3000, function () {
     console.log("Server started at 3000 port for money tracking");
-})
+});
